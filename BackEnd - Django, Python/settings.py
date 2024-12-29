8a98132a43c645d5e975db0b5d85294d6daf4c4c
Shaden Abdelsalam <shadenmohamed784@gmail.com>
2024-12-29 04:47:49 +0200

    Update settings.py

---
+++ BackEnd - Django, Python/settings.py
@@ -0,0 +1,9 @@
+# myproject/settings.py
+INSTALLED_APPS = [
+    ...
+    'myapp',  # Add your app here
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    ...
+]
